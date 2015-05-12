2e19761a4a2078c271c01da37e5c8871721641bb
Laurent ALFONSI <laurent.alfonsi@gmail.com>
2015-05-12 17:20:19 +0200

    multi-players
    to play with 4 players :
      ./a.out a b c d

--- main.c
+++ main.c
@@ -21,7 +21,7 @@
  */
 
 #include <sys/types.h> // ssize_t
-#include <stdlib.h>    // exit(3)
+#include <stdlib.h>    // exit(3), random(3)
 #include <stdio.h>     // fprintf(3), perror(3)
 #include <unistd.h>    // usleep(3), fcntl(2)
 #include <fcntl.h>
@@ -36,29 +36,90 @@
 #define XMAX XBORDER-2
 #define XMIN 1
 
+#define MAXPLAYERS 10
+
 typedef struct {
-	enum { STANDING, JUMPING, KICKING_LEFT, KICKING_RIGHT, FALLING, STICK } state;
+	enum { DISABLED, STANDING, JUMPING, KICKING_LEFT, KICKING_RIGHT, FALLING, STICK } state;
 	bool button_pushed;
-	bool last_kicker;
+	int kick_number;
+	bool direction;
 	ssize_t x;
 	ssize_t y;
 	bool hit;
 	char avatar;
 } Character;
 
+/* Context */
+int last_kick = 0;
+int nb_players = 0;
+
 static int middle_pos (int min, int max, int len)
 {
 	return (max+min)/2-len/2;
 }
 
-static void display_characters(WINDOW *win, const Character *player_1, const Character *player_2)
+static void display_characters(WINDOW *win, const Character players[])
 {
-	mvwprintw(win, 1, 4, "p1: %zd, %zd vs. p2: %zd, %zd",
-			player_1->x, player_1->y, player_2->x, player_2->y);
+	int p;
+	for (p=0;p<MAXPLAYERS;p++)
+	{
+		if (players[p].state==DISABLED) continue;
+		mvwprintw(win, 1, 4+(p*15), "%d(%zd, %zd, %zd)", p, players[p].state, players[p].x, players[p].y);
+	}
+	
 	wrefresh(win);
 }
 
-static void compute_next_step(Character *character, Character *opponent)
+static int check_collision(WINDOW *win, Character *character, Character players[])
+{
+	int p;
+	Character *opponent;
+	
+	for (p=0;p<MAXPLAYERS;p++)
+	{
+		opponent = &players[p];
+
+		if (character == opponent) 
+			continue;
+			
+		if (opponent->state==DISABLED)
+			continue;
+
+		if (   character->x == opponent->x
+		    && character->y == opponent->y)
+		{
+			if (character->kick_number < opponent->kick_number)
+			{
+				character->hit = true;
+				character->avatar=' ';
+				character->state = DISABLED;
+				nb_players--;
+			}
+			else
+			{
+				opponent->hit = true;
+				opponent->avatar=' ';
+				opponent->state = DISABLED;
+				nb_players--;
+			}
+		}
+		if (character->x < XMIN) {
+			character->state = STICK;
+			character->x++;
+		} else if (character->x > XMAX) {
+			character->state = STICK;
+			character->x--;
+		}
+
+		if (character->y >= YMAX)
+		{
+			character->state = STANDING;
+		}
+
+	}
+}
+
+static void compute_next_player_step(Character *character, int kick_right)
 {
 	if (character->button_pushed) {
 		switch (character->state) {
@@ -67,12 +128,11 @@ static void compute_next_step(Character *character, Character *opponent)
 			break;
 		case JUMPING:
 		case FALLING:
-			character->state =
-				(character->x < opponent->x
-					? KICKING_RIGHT
-					: KICKING_LEFT);
-			character->last_kicker = true;
-			opponent->last_kicker  = false;
+			if ( kick_right ) 
+				character->state = KICKING_RIGHT;
+			else
+				character->state = KICKING_LEFT;
+			character->kick_number = last_kick++;
 			break;
 		case KICKING_LEFT:
 		case KICKING_RIGHT:
@@ -109,33 +169,44 @@ static void compute_next_step(Character *character, Character *opponent)
 		character->y++;
 		break;
 	}
-
-	if (   character->x == opponent->x
-	    && character->y == opponent->y)
-		character->hit = true;
-
-	if (character->x < XMIN) {
-		character->state = STICK;
-		character->x++;
-	} else if (character->x > XMAX) {
-		character->state = STICK;
-		character->x--;
-	}
-
-	if (character->y == YMAX)
-		character->state = STANDING;
 }
 
-static void exit_winner(WINDOW *win, int winner)
+static void compute_next_step(WINDOW *win, Character players[])
+{
+	int p;
+
+	/* choose the best direction : TBD */
+	int xaverage=(XMIN+XMAX)/2;
+	
+	for (p=0;p<MAXPLAYERS;p++)
+	{
+		if (players[p].state==DISABLED) continue;
+		compute_next_player_step(&players[p], (players[p].x<xaverage) );
+
+		check_collision(win, &players[p], players);
+	}
+}
+
+static void exit_winner(WINDOW *win, Character players[])
 {
 	const char* msg = "THE ONE WINNER IS";
 	int xpos = middle_pos (XMIN, XMAX, strlen(msg));
 	int ypos = middle_pos (YMIN, YMAX, 0);
+	int winner=-1, p;
+	for (p=0;p<MAXPLAYERS;p++)
+	{
+		if (players[p].state!=DISABLED) 
+		{
+			winner=p;
+			break;
+		}
+	}
+		
 	wattron(win, A_BOLD);
 	wattron(win, COLOR_PAIR(1));
 	mvwprintw(win, ypos, xpos, msg);
 	xpos = middle_pos (XMIN, XMAX, 8);
-	mvwprintw(win, ypos+1, xpos, "PLAYER %d", winner);
+	mvwprintw(win, ypos+1, xpos, "PLAYER %c", players[winner].avatar);
 	wrefresh(win);
 	sleep(5);
 	endwin();
@@ -153,20 +224,26 @@ static void configure_inputs()
 	cbreak();
 }
 
-static void erase_characters(WINDOW *win, const Character *player_1, const Character *player_2)
+static void erase_all_characters(WINDOW *win, const Character players[])
 {
-	mvwaddch(win, player_1->y, player_1->x, ' ');
-	mvwaddch(win, player_2->y, player_2->x, ' ');
+	int p;
+	for (p=0;p<MAXPLAYERS;p++)
+	{
+		if (players[p].state==DISABLED) continue;
+		mvwaddch(win, players[p].y, players[p].x, ' ');
+	}
 }
 
-static void draw_characters(WINDOW *win, const Character *player_1, const Character *player_2)
+static void draw_all_characters(WINDOW *win, const Character players[])
 {
-	wattron(win, COLOR_PAIR(2));
-	mvwaddch(win, player_1->y, player_1->x, player_1->avatar | A_BOLD);
-	wattroff(win, COLOR_PAIR(2));
-	wattron(win, COLOR_PAIR(3));
-	mvwaddch(win, player_2->y, player_2->x, player_2->avatar | A_BOLD);
-	wattroff(win, COLOR_PAIR(3));
+	int p;
+	for (p=0;p<MAXPLAYERS;p++)
+	{
+		if (players[p].state==DISABLED) continue;
+		wattron(win, COLOR_PAIR(2));
+		mvwaddch(win, players[p].y, players[p].x, players[p].avatar | A_BOLD);
+		wattroff(win, COLOR_PAIR(2));
+	}
 	wrefresh(win);
 }
 
@@ -181,21 +258,28 @@ static void color_init()
 	init_pair(5, COLOR_YELLOW, COLOR_BLACK);
 }
 
-int main()
+
+int main(int argc, char **argv)
 {
+
 	WINDOW *title, *board, *debug;
 	int ch;
 	int pos;
 	const char* banner = "*** The ONE Fighting Game ***";
-	Character player_1 = {0};
-	Character player_2 = {0};
-	player_1.y = YMAX;
-	player_2.y = YMAX;
-	player_1.x = XMIN+20;
-	player_2.x = XMAX-20;
-	player_1.avatar = 'o';
-	player_2.avatar = 'x';
-
+	int i, p=0;
+	
+	Character players[MAXPLAYERS] = {0};
+	
+	for (i=1; i<argc; i++)
+	{
+		players[p].y = YMAX;
+		players[p].x = (random()%(XMAX-XMIN))+XMIN;
+		players[p].avatar = *argv[i];
+		players[p].state = STANDING;
+		p++;
+	}
+	nb_players = p;
+	
 	configure_inputs();
 
 	initscr();
@@ -218,32 +302,25 @@ int main()
 	refresh();
 
 	while (1) {
-		display_characters(debug, &player_1, &player_2);
+		display_characters(debug, players);
 
-		erase_characters(board, &player_1, &player_2);
-		compute_next_step(&player_1, &player_2);
-		compute_next_step(&player_2, &player_1);
-		draw_characters(board, &player_1, &player_2);
+		erase_all_characters(board, players);
+		compute_next_step(board, players);
+		draw_all_characters(board, players);
 
-		if (player_1.hit || player_2.hit) {
-			if (player_1.last_kicker)
-				exit_winner(board, 1);
-			else
-				exit_winner(board, 2);
+		if (nb_players==1) 
+		{
+			exit_winner(board, players);
+		
 		}
-
 		usleep(30000);
 
 		ch = getch();
-		switch (ch) {
-			case 'q':
-				player_1.button_pushed = true;
-				break;
-			case 'p':
-				player_2.button_pushed = true;
-				break;
-			default:
-				break;
+		for (p=0; p<MAXPLAYERS; p++)
+		{
+			if (ch == players[p].avatar) {
+				players[p].button_pushed = true;
+			}
 		}
 	}
 	endwin();
