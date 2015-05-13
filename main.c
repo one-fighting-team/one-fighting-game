1754f6a3761c7228e29ace5ee43f3a853fe3c281
Yvan Roux <yvan.roux@free.fr>
2015-05-13 14:38:05 +0200

    Reworked multiplayer mode with chained lists:  - no more static limits  - no parameters  - removed unneeded states

--- main.c
+++ main.c
@@ -28,6 +28,8 @@
 #include <stdbool.h>
 #include <ncurses.h>
 #include <string.h>
+#include <time.h>
+#include <ctype.h>
 
 #define YBORDER 30
 #define XBORDER 120
@@ -36,10 +38,8 @@
 #define XMAX XBORDER-2
 #define XMIN 1
 
-#define MAXPLAYERS 10
-
 typedef struct {
-	enum { DISABLED, STANDING, JUMPING, KICKING_LEFT, KICKING_RIGHT, FALLING, STICK } state;
+	enum { STANDING, JUMPING, KICKING_LEFT, KICKING_RIGHT, FALLING } state;
 	bool button_pushed;
 	int kick_number;
 	bool direction;
@@ -47,75 +47,212 @@ typedef struct {
 	ssize_t y;
 	bool hit;
 	char avatar;
+	int color;
 } Character;
+
+typedef struct Squad{
+	Character *c;
+	struct Squad *next;
+	struct Squad *prev;
+} Squad;
 
 /* Context */
 int last_kick = 0;
-int nb_players = 0;
 
 static int middle_pos (int min, int max, int len)
 {
 	return (max+min)/2-len/2;
 }
 
-static void display_characters(WINDOW *win, const Character players[])
+static void display_character(WINDOW *win, Character player, int p)
 {
-	int p;
-	for (p=0;p<MAXPLAYERS;p++)
-	{
-		if (players[p].state==DISABLED) continue;
-		mvwprintw(win, 1, 4+(p*15), "%d(%zd, %zd, %zd)", p, players[p].state, players[p].x, players[p].y);
-	}
-	
+	mvwprintw(win, 1, 4+(p*15), "%c(%zd, %zd, %zd)", player.avatar,
+		       player.kick_number, player.x, player.y);
 	wrefresh(win);
 }
 
-static int check_collision(WINDOW *win, Character *character, Character players[])
+static void exit_winner(WINDOW *win, Character winner)
 {
-	int p;
-	Character *opponent;
+	const char* msg = "THE ONE WINNER IS";
+	int xpos = middle_pos (XMIN, XMAX, strlen(msg));
+	int ypos = middle_pos (YMIN, YMAX, 0);
+	wattron(win, A_BOLD);
+	wattron(win, COLOR_PAIR(1));
+	mvwprintw(win, ypos, xpos, msg);
+	xpos = middle_pos (XMIN, XMAX, 8);
+	mvwprintw(win, ypos+1, xpos, "PLAYER %c", winner.avatar);
+	wrefresh(win);
+	sleep(5);
+	endwin();
+	exit(0);
+}
+
+static void configure_inputs()
+{
+	int status = fcntl(0, F_SETFL, O_NONBLOCK);
+	if (status < 0) {
+		perror("fcntl");
+		exit(3);
+	}
+
+	cbreak();
+}
+
+static void color_init()
+{
+	start_color();
+	init_pair(1, COLOR_RED, COLOR_BLACK);
+	init_pair(2, COLOR_BLUE, COLOR_BLACK);
+	init_pair(3, COLOR_GREEN, COLOR_BLACK);
+	init_pair(4, COLOR_CYAN, COLOR_BLACK);
+	init_pair(5, COLOR_YELLOW, COLOR_BLACK);
+	init_pair(6, COLOR_MAGENTA, COLOR_BLACK);
+	init_pair(7, COLOR_WHITE, COLOR_BLACK);
+}
+
+static void add_player(Squad **l, Character *c)
+{
+	Squad *p = malloc(sizeof(Squad));
+	if (NULL == p) exit(1);
+	p->c = c;
+	p->prev = NULL;
+	p->next = *l;
+	if (*l) (*l)->prev = p;
 	
-	for (p=0;p<MAXPLAYERS;p++)
+	*l = p;
+}
+
+static void remove_hitted_fighters(Squad **list)
+{
+	Squad *l = *list;
+	while (l)
 	{
-		opponent = &players[p];
-
-		if (character == opponent) 
-			continue;
-			
-		if (opponent->state==DISABLED)
-			continue;
-
-		if (   character->x == opponent->x
-		    && character->y == opponent->y)
+		if (l->c->hit)
 		{
-			if (character->kick_number < opponent->kick_number)
-			{
-				character->hit = true;
-				character->avatar=' ';
-				character->state = DISABLED;
-				nb_players--;
-			}
+			if (l->next) l->next->prev = l->prev;
+			if (l->prev) l->prev->next = l->next;
+			else *list = l->next;
+			free(l->c);
+			free(l);
+		}
+		l = l->next;
+	}
+}
+
+static void draw_fighters(WINDOW *win, Squad *l)
+{
+	while (l)
+	{
+		wattron(win, COLOR_PAIR(l->c->color % 8));
+		mvwaddch(win, l->c->y, l->c->x, l->c->avatar | A_BOLD);
+		wattroff(win, COLOR_PAIR(l->c->color % 8));
+		l = l->next;
+	}
+	wrefresh(win);
+
+}
+
+static int erase_fighters(WINDOW *win, Squad *l)
+{
+	int cpt = 0;
+	while (l)
+	{
+		mvwaddch(win, l->c->y, l->c->x, ' ');
+		l = l->next;
+		cpt++;
+	}
+	return cpt;
+}
+
+static Squad *select_fighters(WINDOW *win, WINDOW *win2)
+{
+	// TODO: Implement it as a timer. 
+
+	Squad *list = NULL;
+	int xpos, ypos, ch, cpt = 0;
+	const char *msg = "Choose your avatar Figthers (Enter to stop)";
+
+	xpos = middle_pos(XMIN, XMAX, strlen(msg));
+	ypos = middle_pos(YMIN, YMAX, 0);
+	wattron(win, A_BOLD);
+	mvwaddstr(win, ypos, xpos, msg);
+	wrefresh(win);
+
+	while(1){
+		Character *c;
+		ch = getch();
+		if (ch == '\n') break;
+		if (isascii(ch) && ch != ' ' && ch != '\t')
+		{
+			cpt++;
+			c = (Character *)malloc(sizeof(Character));
+			c->y = YMAX;
+			c->x = (random()%(XMAX-XMIN))+XMIN;
+			c->avatar = ch;
+			c->state = STANDING;
+			c->kick_number = 0;
+			c->color = cpt;
+			add_player(&list, c);
+			display_character(win2, *c, cpt);
+		}
+	}
+
+	wclear(win);
+	box(win, 0, 0);
+	return list;
+}
+
+static void debug_list(WINDOW *win, Squad *l)
+{
+	int cpt = 0;
+	wclrtoeol(win);
+	box(win, 0, 0);
+	while(l)
+	{
+		cpt++;
+		display_character(win, *l->c, cpt);
+		l = l->next;
+	}
+}
+
+static void check_button(Squad *l)
+{
+	int ch = getch();
+	while (l)
+	{
+		if (ch == l->c->avatar)
+			l->c->button_pushed = true;
+		l = l->next;
+	}
+}
+
+static void check_collision(WINDOW *win, Character *c, Squad *l)
+{
+	while (l)
+	{
+		if (c->x < XMIN) {
+			c->x++;
+		} else if (c->x > XMAX) {
+			c->x--;
+		}
+
+		if (c->y >= YMAX) c->state = STANDING;
+
+		if (c == l->c) 
+		{
+			l = l->next;
+			continue;
+		}
+
+		if (c->x == l->c->x && c->y == l->c->y)
+		{
+			if (c->kick_number < l->c->kick_number)
+				c->hit = true;
 			else
-			{
-				opponent->hit = true;
-				opponent->avatar=' ';
-				opponent->state = DISABLED;
-				nb_players--;
-			}
-		}
-		if (character->x < XMIN) {
-			character->state = STICK;
-			character->x++;
-		} else if (character->x > XMAX) {
-			character->state = STICK;
-			character->x--;
+				l->c->hit = true;
 		}
 
-		if (character->y >= YMAX)
-		{
-			character->state = STANDING;
-		}
-
+		l = l->next;
 	}
 }
 
@@ -136,7 +273,6 @@ static void compute_next_player_step(Character *character, int kick_right)
 			break;
 		case KICKING_LEFT:
 		case KICKING_RIGHT:
-		case STICK:
 			// TODO
 			break;
 		}
@@ -164,122 +300,38 @@ static void compute_next_player_step(Character *character, int kick_right)
 		character->y++;
 		character->x++;
 		break;
-	case STICK:
-		character->state = FALLING;
-		character->y++;
-		break;
 	}
 }
 
-static void compute_next_step(WINDOW *win, Character players[])
+static void compute_next_step(WINDOW *win, Squad *fighters, int nbp)
 {
-	int p;
-
-	/* choose the best direction : TBD */
-	int xaverage=(XMIN+XMAX)/2;
-	
-	for (p=0;p<MAXPLAYERS;p++)
+	Squad *i = fighters;
+	while (i)
 	{
-		if (players[p].state==DISABLED) continue;
-		compute_next_player_step(&players[p], (players[p].x<xaverage) );
-
-		check_collision(win, &players[p], players);
-	}
-}
-
-static void exit_winner(WINDOW *win, Character players[])
-{
-	const char* msg = "THE ONE WINNER IS";
-	int xpos = middle_pos (XMIN, XMAX, strlen(msg));
-	int ypos = middle_pos (YMIN, YMAX, 0);
-	int winner=-1, p;
-	for (p=0;p<MAXPLAYERS;p++)
-	{
-		if (players[p].state!=DISABLED) 
+		Squad *j = fighters;
+		int nbr = 0;
+		while (j)
 		{
-			winner=p;
-			break;
+			if (i->c->x < j->c->x) nbr++;
+			j = j->next;
 		}
-	}
-		
-	wattron(win, A_BOLD);
-	wattron(win, COLOR_PAIR(1));
-	mvwprintw(win, ypos, xpos, msg);
-	xpos = middle_pos (XMIN, XMAX, 8);
-	mvwprintw(win, ypos+1, xpos, "PLAYER %c", players[winner].avatar);
-	wrefresh(win);
-	sleep(5);
-	endwin();
-	exit(winner);
-}
 
-static void configure_inputs()
-{
-	int status = fcntl(0, F_SETFL, O_NONBLOCK);
-	if (status < 0) {
-		perror("fcntl");
-		exit(3);
-	}
-
-	cbreak();
-}
-
-static void erase_all_characters(WINDOW *win, const Character players[])
-{
-	int p;
-	for (p=0;p<MAXPLAYERS;p++)
-	{
-		if (players[p].state==DISABLED) continue;
-		mvwaddch(win, players[p].y, players[p].x, ' ');
+		compute_next_player_step(i->c, nbr >= nbp/2);
+		check_collision(win, i->c, i);
+		i = i->next;
 	}
 }
-
-static void draw_all_characters(WINDOW *win, const Character players[])
-{
-	int p;
-	for (p=0;p<MAXPLAYERS;p++)
-	{
-		if (players[p].state==DISABLED) continue;
-		wattron(win, COLOR_PAIR(2));
-		mvwaddch(win, players[p].y, players[p].x, players[p].avatar | A_BOLD);
-		wattroff(win, COLOR_PAIR(2));
-	}
-	wrefresh(win);
-}
-
-
-static void color_init() 
-{
-	start_color();
-	init_pair(1, COLOR_RED, COLOR_BLACK);
-	init_pair(2, COLOR_BLUE, COLOR_BLACK);
-	init_pair(3, COLOR_GREEN, COLOR_BLACK);
-	init_pair(4, COLOR_CYAN, COLOR_BLACK);
-	init_pair(5, COLOR_YELLOW, COLOR_BLACK);
-}
-
 
 int main(int argc, char **argv)
 {
 
 	WINDOW *title, *board, *debug;
-	int ch;
 	int pos;
 	const char* banner = "*** The ONE Fighting Game ***";
-	int i, p=0;
+	int nbp = 0;
 	
-	Character players[MAXPLAYERS] = {0};
-	
-	for (i=1; i<argc; i++)
-	{
-		players[p].y = YMAX;
-		players[p].x = (random()%(XMAX-XMIN))+XMIN;
-		players[p].avatar = *argv[i];
-		players[p].state = STANDING;
-		p++;
-	}
-	nb_players = p;
-	
+	Squad *fighters = NULL;
+
 	configure_inputs();
 
 	initscr();
@@ -301,27 +353,21 @@ int main(int argc, char **argv)
 	mvwaddstr(title, 1, pos, banner);
 	refresh();
 
+	fighters = select_fighters(board, debug);
+
 	while (1) {
-		display_characters(debug, players);
+		debug_list(debug, fighters);
 
-		erase_all_characters(board, players);
-		compute_next_step(board, players);
-		draw_all_characters(board, players);
+		nbp = erase_fighters(board, fighters);
+		compute_next_step(board, fighters, nbp);
+		remove_hitted_fighters(&fighters);
+		draw_fighters(board, fighters);
 
-		if (nb_players==1) 
-		{
-			exit_winner(board, players);
+		if (nbp == 1) exit_winner(board, *fighters->c);
 		
-		}
 		usleep(30000);
 
-		ch = getch();
-		for (p=0; p<MAXPLAYERS; p++)
-		{
-			if (ch == players[p].avatar) {
-				players[p].button_pushed = true;
-			}
-		}
+		check_button(fighters);
 	}
 	endwin();
 }
